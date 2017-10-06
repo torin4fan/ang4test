8f0511d0ee7bead0f3541c72bbde0908d1c0ffe0
admin <maksym.kolisnyk@wishabi.com>
2017-10-07 00:19:46 +0300

    simple redux example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+interface AppState {
+  message: string;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'tr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  message$: Observable<string>;
   title = 'app';
+
+  constructor(private store: Store<AppState>) {
+    this.message$ = this.store.select('message');
+  }
+
+  spanisMessage() {
+    this.store.dispatch({type: 'SPANISH'});
+  }
+
+  frenchMessage() {
+    this.store.dispatch({type: 'FRENCH'});
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+
 import { AppComponent } from './app.component';
+import { simpleReducer } from './_reducers/simple.reduser';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({
+      message: simpleReducer
+    }),
+    StoreDevtoolsModule.instrument()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
