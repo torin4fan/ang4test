833aa298c5932ba65e494e2a5fa9a7be1d07d62b
Torin4fan <torin4fan@gmail.com>
2017-11-21 21:14:44 +0200

    fix routing

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,40 +1,34 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-import { AuthGuard } from './core/services/auth-guard.service';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './modules/auth/login-page/login-page.component';
 import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
-import { AddEditCourseComponent } from './modules/courses/add-edit-course/add-edit-course.component';
-import { CoursesListComponent } from './modules/courses/courses-list/courses-list.component';
 
 
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'courses',
     component: AppComponent,
     children: [
       {
         path: '',
         loadChildren: './modules/courses/courses.module#CoursesModule'
       },
-      {
-        path: 'login',
-        component: LoginPageComponent
-      },
-      {
-        path: '**',
-        component: PageNotFoundComponent,
-        pathMatch: 'full'
-      }
     ]
-  }
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  { path: '',   redirectTo: '/courses', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
   ],
   exports: [RouterModule]
 })
