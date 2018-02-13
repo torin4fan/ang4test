be5122df26df5eb4ab87800728cd403db3dd1a82
Maksym Kolisnyk <Maksym_Kolisnyk@epam.com>
2018-02-13 17:17:36 +0200

    mongoDb

--- mock-server/app.js
+++ mock-server/app.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(db.url, (error, database) => {
+  const myAwesomeDB = database.db('hazh');
+  if (error) return console.log(error);
+  require('./app/routes')(app, myAwesomeDB);
+});
 
 app.get('/courses', function (req, res) {
-  var courses = [];
-  var result = null;
+  let courses = [];
+  let result = null;
 
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
   courses = require('./courses.json');
 
-  if(req.query.id) {
+  if (req.query.id) {
     result = courses.filter(function (response) {
       return response.id === +req.query.id;
     });
